1367fe6f1f58c6c7e182ece4e0e62ec82a947218
Max de Vocht <maxdevocht@hotmail.com>
2024-03-04 14:26:16 +0100

    Update js
    added skeleton

---
+++ main.js
@@ -0,0 +1,6 @@
+const inputFieldEl = document.getElementById("input-field")
+const addBtn = document.getElementById("add-btn")
+
+inputFieldEl.addEventListener("click", function() {
+    console.log(inputFieldEl.value)
+})
