9ae166be06b51ab68ae6f3893ce11a637d681132
Max de Vocht <maxdevocht@hotmail.com>
2024-03-05 11:42:19 +0100

    Update js
    Added firebase realtime database

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://playground-8e380-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
+
 const inputFieldEl = document.getElementById("input-field")
 const addBtn = document.getElementById("add-btn")
 
