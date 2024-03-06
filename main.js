6d44648913d68a1137e937e1e158fb9317350571
Max de Vocht <maxdevocht@hotmail.com>
2024-03-06 11:35:54 +0100

    Update js
    Changed object.values to entries

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ addBtn.addEventListener("click", function() {
 })
 
 onValue(shoppingListInDB, function(snapshot) {
-    let itemsArray = Object.values(snapshot.val())
+    let itemsArray = Object.entries(snapshot.val())
     clearShoppingListEl()
     for (let i = 0; i < itemsArray.length; i++) {
         let currentItem = itemsArray[i]
@@ -37,7 +37,6 @@ function clearShoppingListEl() {
 
 function clearInputFieldEl() {
     inputFieldEl.value = ""
-    
 }
 
 function appendListItem(itemValue) {
