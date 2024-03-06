df8c99c636cc7a30a8740abd36cedf4ebe3af310
Max de Vocht <maxdevocht@hotmail.com>
2024-03-06 14:46:39 +0100

    Finished

--- main.js
+++ main.js
@@ -1,23 +1,35 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
 import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
+
+
 const appSettings = {
     databaseURL: "https://playground-8e380-default-rtdb.europe-west1.firebasedatabase.app/"
 }
+
+
+
 
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
 const shoppingListInDB = ref(database, "shoppingList")
 
+
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 const shoppingListEl = document.getElementById("shopping-list")
+
+
+
 
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
     push(shoppingListInDB, inputValue)
     clearInputFieldEl()
 })
+
+
+
 
 onValue(shoppingListInDB, function(snapshot) {
 
@@ -39,13 +51,22 @@ onValue(shoppingListInDB, function(snapshot) {
 
 })
 
+
+
+
 function clearShoppingListEl() {
     shoppingListEl.innerHTML = ""
 }
 
+
+
+
 function clearInputFieldEl() {
     inputFieldEl.value = ""
 }
+
+
+
 
 function appendItemToShoppingListEl(item) {
     let itemID = item[0]
@@ -62,4 +83,20 @@ function appendItemToShoppingListEl(item) {
 
     shoppingListEl.append(newEl)
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
